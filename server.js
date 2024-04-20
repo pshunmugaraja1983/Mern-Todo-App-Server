b56e870b80cb0b93f39f2439ea2d69de41b8e517
pshunmugaraja1983 <pshunmugaraja1983@gmail.com>
2024-04-20 06:19:18 +0530

    1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.NODEJS_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // Connect to MongoDB
