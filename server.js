6aa21bc2f79f4bfc9df8c28d9873a136b65d2441
pshunmugaraja1983 <pshunmugaraja1983@gmail.com>
2024-04-25 17:44:24 +0530

    1

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://psr1983:67Y2NyqRdSTHr0mZ@cluster0.jg6xd2k.mongodb.net/app_027?retryWrites=true&w=majority&appName=Cluster0"
+
 app.use(cors());
 app.use(express.json());
 // Connect to MongoDB
 
-const mongodb_url = process.env.MONGODB_URI
 
-mongoose.connect(mongodb_url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // Define routes and middleware
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -41,11 +43,11 @@ app.post('/todos', async (req, res) => {
 });
 // Update an existing todo
 app.put('/todos/:id', async (req, res) => {
-  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: false });
   res.json(updatedTodo);
 });
 // Delete a todo
 app.delete('/todos/:id', async (req, res) => {
-  await Todo.findByIdAndRemove(req.params.id);
+  await Todo.findByIdAndDelete(req.params.id);
   res.json({ message: 'Todo deleted successfully' });
 });
